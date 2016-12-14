6f45a18aeba7a56cdfd38d48a82f67e8a1245173
张沿伍 <272352601@qq.com>
2016-12-14 10:59:59 +0800

    新建

---
+++ app/src/main/java/com/zhang/myclock/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zhang.myclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        Toast.makeText(this, "haha", Toast.LENGTH_SHORT).show();
+    }
+}
